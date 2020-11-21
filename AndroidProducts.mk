d7fbbcc72a27c45e95414fc5e218d6119d8a9183
hemantsachdeva <hksachdeva007@gmail.com>
2020-11-21 11:57:17 +0530

    laurel_sprout: Add lunch choices
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/evolution_laurel_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_laurel_sprout-user \
+    evolution_laurel_sprout-userdebug \
+    evolution_laurel_sprout-eng
